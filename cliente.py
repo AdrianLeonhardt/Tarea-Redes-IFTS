9d2279787b5d6256c81f1faa602b10e0eb13b2fa
Adrian Leonhardt <55300994+AdrianLeonhardt@users.noreply.github.com>
2025-06-08 13:13:26 -0300

    agregado de comentarios

--- cliente.py
+++ cliente.py
@@ -1,17 +1,20 @@
-import socket
-import threading
+import socket # Para crear la conexión de red (cliente-servidor)
+import threading # Para ejecutar múltiples tareas al mismo tiempo (hilos)
 
+# Dirección IP y puerto del servidor al que el cliente se conectará
 HOST="127.0.0.1"
 PORT= 12345
 
-# Inicializar socket del cliente
+# Inicializa el socket del cliente como un socket TCP/IP
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Conecta el cliente al servidor especificado por HOST y PORT
 cliente.connect((HOST, PORT))
 
 # Función para recibir mensajes del servidor
 def recibir_mensajes():
     while True:
         try:
+            # Recibe hasta 1024 bytes desde el servidor y decofica bytes a string
             mensaje = cliente.recv(1024).decode('utf-8')
             print(mensaje)
         except:
@@ -23,8 +26,10 @@ def recibir_mensajes():
 def enviar_mensajes():
     while True:
         try:
+            # Espera al input del usuario
             mensaje = input()
             cliente.send(mensaje.encode('utf-8'))
+            # Si el usuario escribe "/exit", termina la comunicación
             if mensaje == "/exit":
                 break
         except:
